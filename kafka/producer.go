e163d1963252f54111ab9e8d21773ed0378845b2
llsdah <77381506+llsdah@users.noreply.github.com>
2025-05-03 15:08:03 +0900

    version 0.1

--- kafka/producer.go
+++ kafka/producer.go
@@ -3,16 +3,13 @@ package kafka
 import (
 	"context"
 	"log"
+	"sync"
 
 	"github.com/segmentio/kafka-go"
 )
 
-// kafka 메시지 전송 produce 객체 설정
-var writer = &kafka.Writer{
-	Addr:     kafka.TCP("localhost:9092"), // 브로커 주소
-	Topic:    "gateway-events",            // 기본메시지 토픽
-	Balancer: &kafka.LeastBytes{},         // 로드밸런싱 방식
-}
+var writerMap map[string]*kafka.Writer // 각 브로커 보관
+var writerMu sync.Mutex                // 쓰레드 관리 lock 사용
 
 // 고정 토픽
 func ProduceMessage(ctx context.Context, key, value string) error {
@@ -20,6 +17,14 @@ func ProduceMessage(ctx context.Context, key, value string) error {
 		Key:   []byte(key),
 		Value: []byte(value),
 	}
+
+	// kafka 메시지 전송 produce 객체 설정
+	writer := &kafka.Writer{
+		Addr:     kafka.TCP("localhost:9092"), // 브로커 주소
+		Topic:    "gateway-events",            // 기본메시지 토픽
+		Balancer: &kafka.LeastBytes{},         // 로드밸런싱 방식
+	}
+
 	// 비동기 전송
 	err := writer.WriteMessages(ctx, msg)
 	if err != nil {
@@ -50,11 +55,13 @@ func ProduceToTopicWithBrokers(ctx context.Context, brokers []string, topic, key
 
 	for i := 0; i < attempt; i++ {
 
+		log.Printf("brokers : %+v", brokers)
 		writer := kafka.NewWriter(kafka.WriterConfig{
 			Brokers:  brokers,
 			Topic:    topic,
 			Balancer: &kafka.LeastBytes{},
 		})
+
 		err = writer.WriteMessages(ctx, msg)
 		writer.Close()
 
@@ -66,4 +73,25 @@ func ProduceToTopicWithBrokers(ctx context.Context, brokers []string, topic, key
 
 	return err
 
+}
+
+func initKafkaWriters(brokers []string, topic string) {
+	writerMu.Lock()
+	defer writerMu.Unlock()
+
+	for _, broker := range brokers {
+		if _, exists := writerMap[broker]; !exists {
+			writer := getKafkaWriter(broker, topic)
+			writerMap[broker] = writer
+			log.Printf("Initialized writer for broker %s", broker)
+		}
+	}
+}
+
+func getKafkaWriter(broker string, topic string) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:     kafka.TCP(broker),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
 }
